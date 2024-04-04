fed74ec0113b57bca5c71e1209d1686e39823ea7
p0nch000 <A01198400@tec.mx>
2024-04-04 11:46:38 -0600

    Initial repo config

---
+++ api.js
@@ -0,0 +1,36 @@
+import { sequelize } from "./config/db.js";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+//import "./models/associations.js";
+//import router from "./routes/routes.js";
+
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+        methods: "GET, POST, PUT, DELETE",
+        allowedHeaders: "Content-Type, Authorization",
+    })
+);
+
+//app.use(router);
+
+
+async function main() {
+    await sequelize.sync({ force: false });
+    console.log("Connected to DB")
+
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+    
+}
+main();
+
+export default app;
