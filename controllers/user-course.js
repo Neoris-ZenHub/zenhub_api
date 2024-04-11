ac2616888d3dd3c01903f573f66d6ef04a431c9b
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-04-11 09:04:48 -0600

    Push por sia caso

--- controllers/user-course.js
+++ controllers/user-course.js
@@ -1,5 +1,6 @@
 import { User } from "../models/users.js";
 import { Course } from "../models/courses.js";
+import { Progress } from "../models/progress.js";
 import Sequelize from "sequelize";
 
 //Get Courses from a User
@@ -14,6 +15,12 @@ export const getUserCourses = async (req, res) => {
                 model: Course,
                 attributes: ['name'], // Specify the attributes you want to include
                 through: { attributes: [] },
+                include: [{
+                    model: Progress,
+                    where: { _id_user: _id_user },
+                    attributes: ['percentage'],
+                    required: false,
+                }],
             }]
         });
 
@@ -22,9 +29,13 @@ export const getUserCourses = async (req, res) => {
         }
 
         res.status(200).send({
-            message: "Course created successfully",
-            courses: courses.Courses,
+            message: "Courses retrieved successfully",
+            courses: courses.Courses.map(course => ({
+                name: course.name,
+                percentage: course.Progresses.length > 0 ? course.Progresses[0].percentage : 0,
+            })),
         });
+        
     } catch (error) {
         console.error("Error during user-path retrieval:", error);
         res.status(500).send({
