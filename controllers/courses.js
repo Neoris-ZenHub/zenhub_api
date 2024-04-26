8b4fd312a2595c5585bb24f3792510fda8cedf34
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-04-26 17:15:29 -0600

    Get courses from a path

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,6 +2,7 @@ import { User } from "../models/users.js";
 import { Course } from "../models/courses.js";
 import Sequelize from "sequelize";
 import { UserPath } from "../models/user-paths.js";
+import { Path } from "../models/paths.js";
 
 //Create a new Course
 export const createCourse = async (req,res) => {
@@ -152,3 +153,41 @@ export const updateCourse = async (req, res) => {
         }
     }
 };
+export const getCoursesByPath = async (req, res) => {
+    try{
+        const name_path = req.query.name_path;
+
+        const decodedPath = decodeURI(name_path);
+
+        const path = await Path.findOne({
+            where: { name: decodedPath }
+        });
+
+        if (!path) {
+            return res.status(404).send({ message: "Path not found" });
+        }
+
+        const courses = await Course.findAll({
+            where: { _id_path: path._id_path }
+        })
+
+        res.status(200).send({
+            message: "Courses retrieved successfully",
+            courses: courses,
+        });
+
+    } catch (error) {
+        console.error("Error during course retrieval:", error);
+
+        if (error instanceof Sequelize.ValidationError) {
+            return res
+                .status(400)
+                .send({ message: "Validation error", errors: error.errors });
+        } else {
+            return res.status(500).send({
+                message: "An unexpected error occurred",
+                error: error.message,
+            });
+        }
+    }
+}
